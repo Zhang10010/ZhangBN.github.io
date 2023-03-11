1ea50778f6661c78c4ea93298bdd8a1eb10a2ad1
Zhang10010 <55727338+Zhang10010@users.noreply.github.com>
2023-03-11 13:20:33 -0500

    Add files via upload

---
+++ RP2040/secrets.py
@@ -0,0 +1,3 @@
+Tufts_Wireless = {'ssid':'Tufts_Wireless','pass':''}
+Tufts_eecs = {'ssid':'tufts_eecs','pass':'foundedin1883'}
+home = {'ssid':'zhaowenrui','pass':'zhangboning'}
---
+++ RP2040/valueMath.py
@@ -0,0 +1,7 @@
+def negative(array):
+    for i in range(len(array)):
+        array[i]=-array[i]
+        
+def add(array,x):
+    for i in range(len(array)):
+        array[i]=array[i]+x
